3e24f3f4d3f561b33c9fd1cd111ad311b481a8f5
PhuNH <1079742+PhuNH@users.noreply.github.com>
2021-12-09 15:03:36 +0100

    Day 9 2021

---
+++ 2021/9/__init__.py
@@ -0,0 +1,53 @@
+import os
+
+from libs.vector import Vector, Area
+
+
+def main(input_path: str = ''):
+    with open(input_path) as f:
+        area = Area([[int(h) for h in line] for line in f.read().strip().split('\n')])
+
+        def is_higher_than(v: Vector):
+            return lambda m: area[m.y][m.x] > area[v.y][v.x]
+
+        risk_sum = 0
+        processing = []
+        for y in range(area.height):
+            for x in range(area.width):
+                current = Vector(x, y)
+                if all(map(is_higher_than(current), area.adjacents(current))):
+                    risk_sum += 1 + area[y][x]
+                    processing.append(Vector(x, y))
+        print(risk_sum)
+
+        basin_points = set(processing)
+        basin_size = 0
+        basin_sizes = []
+        while True:
+            current = processing.pop()
+            adjacents = area.adjacents(current)
+            higher = set(filter(is_higher_than(current), adjacents))
+            if higher == adjacents:
+                if basin_size > 0:
+                    basin_sizes.append(basin_size)
+                basin_size = 1
+            else:
+                basin_size += 1
+            next_level = list(filter(lambda v: area[v.y][v.x] < 9 and v not in basin_points, higher))
+            basin_points = basin_points.union(next_level)
+            processing.extend(next_level)
+
+            if len(processing) == 0:
+                if basin_size > 0:
+                    basin_sizes.append(basin_size)
+                break
+        basin_sizes.sort(reverse=True)
+        print(basin_sizes[0] * basin_sizes[1] * basin_sizes[2])
+
+
+if __name__ == '__main__':
+    input_file = './input'
+    if os.path.isfile(input_file):
+        main(input_file)
+    else:
+        main()
--- libs/vector.py
+++ libs/vector.py
@@ -1,11 +1,59 @@
+import copy
+
+
 class Vector:
     def __init__(self, *elements):
         self._data = elements
         self.x = elements[0]
         self.y = elements[1]
 
+    @property
+    def up(self):
+        return Vector(self.x, self.y-1)
+
+    @property
+    def down(self):
+        return Vector(self.x, self.y+1)
+
+    @property
+    def left(self):
+        return Vector(self.x-1, self.y)
+
+    @property
+    def right(self):
+        return Vector(self.x+1, self.y)
+
+    @property
+    def adjacents(self):
+        return {self.up, self.down, self.left, self.right}
+
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self._data == other._data
 
     def __hash__(self):
         return hash(self._data)
+
+    def __str__(self):
+        return self._data.__str__()
+
+
+class Area:
+    def __init__(self, data: [[]]):
+        self._data = data
+        self.width = len(data[0])
+        self.height = len(data)
+
+    def __getitem__(self, item):
+        return self._data.__getitem__(item)
+
+    def adjacents(self, point: Vector) -> set[Vector]:
+        result = copy.deepcopy(point.adjacents)
+        if point.x == 0:
+            result.remove(point.left)
+        elif point.x == self.width-1:
+            result.remove(point.right)
+        if point.y == 0:
+            result.remove(point.up)
+        elif point.y == self.height-1:
+            result.remove(point.down)
+        return result
