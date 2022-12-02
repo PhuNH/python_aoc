a3c0871ad7435d688178f6b2bb61a67b96ee3596
PhuNH <1079742+PhuNH@users.noreply.github.com>
2022-12-02 17:03:04 +0100

    Make a 'path' module to hold Path class for now

--- 2021/12/__init__.py
+++ 2021/12/__init__.py
@@ -1,21 +1,7 @@
-import copy
 import os
 from collections import deque
 
-
-class Path:
-    def __init__(self, start):
-        self.nodes = [start]
-        self.node_set = set(self.nodes)
-
-    def add_node(self, node):
-        path = copy.deepcopy(self)
-        path.nodes.append(node)
-        path.node_set = set(path.nodes)
-        return path
-
-    def __contains__(self, item):
-        return item in self.node_set
+from libs.path import Path
 
 
 class TwicePath(Path):
